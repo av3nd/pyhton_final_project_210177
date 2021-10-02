60617a1ae6714c3b65d66880e7353680c4a2ef44
avend tabdar <avendtabdar@gmail.com>
2021-10-02 10:40:39 +0545

    Adding styling for treeview

--- recordSaver.py
+++ recordSaver.py
@@ -1,1 +1,20 @@
-print("testing the git hub")
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+root.title('Management System')
+root.geometry("1000x500")
+
+
+# style for treeview
+style = ttk.Style()
+style.theme_use('default')
+
+style.configure("Treeview",
+    background='#D3D3D3',
+    foreground="black",
+    rowheight=25,
+    fieldbackground="#D3D3D3")
+
+style.map('Treeview',
+          background=[('selected',"#347083")])
