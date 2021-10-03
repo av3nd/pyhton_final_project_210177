50dc171f2ff439b43dff473dc5e6be256aa69565
avend tabdar <avendtabdar@gmail.com>
2021-10-03 14:57:27 +0545

    Login page function added

--- loginPage.py
+++ loginPage.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from tkinter import messagebox
+import os
 
 admin = Tk()
 admin.title("Login Page")
@@ -6,21 +8,38 @@ admin.geometry("305x210")
 admin.iconbitmap('key.ico')
 admin.config(bg="lightgrey")
 
-admin_frame = LabelFrame(admin, text="Enter Username and Password",font=('Consolas',11))
+#Function
+def ok():
+    uname=admin_entry.get()
+    password=admin_entry1.get()
+    if(uname==""and password==""):
+        messagebox.showinfo("Error","Blank Not Allowed")
+    elif(uname=="admin"and password=="admin"):
+        messagebox.showinfo("Login Success","Username and Password correct!!")
+        admin.withdraw()
+        os.system("treeview_app.py")
+
+
+    else:
+        messagebox.showinfo("Error","Incorrect Username and Password")
+
+#Admin Frame
+admin_frame = LabelFrame(admin, text="Enter Username and Password",font=('Consolas',11),bg='#b0ebea')
 admin_frame.place(x=1, y=0, width=302, height=205)
+# admin_frame.config(bg=#34b1eb)
 
-
-admin_lbl = Label(admin_frame,text="Username: ",font=('Consolas',11))
-admin_lbl.grid(row=0, column=0, padx=20, pady=20)
+#Inside Admin Frame
+admin_lbl=Label(admin_frame,text="Username: ",font=('Consolas',12),bg='#b0ebea')
+admin_lbl.grid(row=0, column=0, padx=15, pady=15)
 admin_entry = Entry(admin_frame,show='*')
-admin_entry.grid(row=0, column=1, padx=20, pady=20)
+admin_entry.grid(row=0, column=1, padx=15, pady=15)
 
-admin_lbl1 = Label(admin_frame,text="Password: ",font=('Consolas',11))
-admin_lbl1.grid(row=1, column=0, padx=20, pady=20)
+admin_lbl1 = Label(admin_frame,text="Password: ",font=('Consolas',12),bg='#b0ebea')
+admin_lbl1.grid(row=1, column=0, padx=15, pady=15)
 admin_entry1 = Entry(admin_frame,show='*')
-admin_entry1.grid(row=1, column=1, padx=20, pady=20)
+admin_entry1.grid(row=1, column=1, padx=15, pady=15)
 
-admin_button = Button(admin_frame, text="Login",font=('Consolas',11),cursor='hand2',bg="lightgrey")
+admin_button = Button(admin_frame, text="Login",font=('Consolas',11),cursor='hand2',bg="lightgrey",command=ok)
 admin_button.grid(row=2,column=1,padx=20,pady=20)
 
 admin.mainloop()
